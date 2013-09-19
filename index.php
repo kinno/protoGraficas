27f892caa2706c19a9222e97765a83e8ce2650ec
admin <admin@user>
2013-09-19 13:32:03 -0500

    inicio

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div id="contenedor">
+            <div id="menu">
+                <ul>
+                    <li>Selecciona una tabla: 
+                        <select>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                        </select>
+                    </li>
+                    <li>Selecciona el campo: 
+                        <select>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                        </select>
+                    </li>
+                    <li>Selecciona el tipo de gráfica: 
+                        <select>
+                            <option>Barras</option>
+                            <option>Radar</option>
+                            <option>Pie</option>
+                            <option>Series lineales</option>
+                        </select>
+                    </li>
+                </ul>
+            </div>
+            <div id="resultado">
+                
+            </div>
+        </div>
+    </body>
+</html>
